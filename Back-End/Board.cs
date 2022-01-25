f6a2a57c5fda58c904ee229905e4eee7c8b36411
malibak4 <malibak4@students.kennesaw.edu>
2022-01-25 15:06:31 -0500

    Created definitions for the pieces and board and added piece ID numbers

---
+++ Back-End/Board.cs
@@ -0,0 +1,64 @@
+﻿using Pieces;
+using System;
+public class Board {
+	private Piece[,] board;
+	private int dim;
+	private EmptySquare e;
+	//Default 8x8 chessboard
+	public Board()
+	{
+		e = new EmptySquare();
+		board = new Piece[8, 8] {
+			{ new Rook(Color.Black, 0), new Knight(Color.Black, 0), new Bishop(Color.Black, 0), new Queen(Color.Black),
+			new King(Color.Black), new Bishop(Color.Black, 1), new Knight(Color.Black, 1), new Rook(Color.Black, 1) },
+			{ new Pawn(Color.Black, 0), new Pawn(Color.Black, 1), new Pawn(Color.Black, 2), new Pawn(Color.Black, 3),
+			new Pawn(Color.Black, 4), new Pawn(Color.Black, 5), new Pawn(Color.Black, 6), new Pawn(Color.Black, 7) },
+			{ e, e, e, e, e, e, e, e }, {e, e, e, e, e, e, e, e }, { e, e, e, e, e, e, e, e }, {e, e, e, e, e, e, e, e },
+			{ new Pawn(Color.White, 0), new Pawn(Color.White, 1), new Pawn(Color.White, 2), new Pawn(Color.White, 3),
+			new Pawn(Color.White, 4), new Pawn(Color.White, 5), new Pawn(Color.White,6 ), new Pawn(Color.White, 7) },
+			{ new Rook(Color.White, 0), new Knight(Color.White, 0), new Bishop(Color.White, 0), new Queen(Color.White),
+			new King(Color.White), new Bishop(Color.White, 1), new Knight(Color.White, 1), new Rook(Color.White, 1) }};
+		dim = 8;
+	}
+	//Moves a piece at the coordinates [PieceX, PieceY] to the space on the board with the coordinates [DestinationX, DestinationY]
+	public void Move(int PieceX, int PieceY, int DestinationX, int DestinationY) {
+		board[DestinationX, DestinationY] = board[PieceX, PieceY];
+		board[PieceX, PieceY] = e;//Target's previous space is now empty
+	}
+	public Piece[,] getBoard() {
+		return board;
+	}
+	public void Print() {
+		/* Visualization of how the default board is arranged:
+		char[,] textBoard = new char[8, 8] {
+			{ 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' },
+			{ 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+			{ '-', '-', '-', '-', '-', '-', '-', '-' },
+			{ '-', '-', '-', '-', '-', '-', '-', '-' },
+			{ '-', '-', '-', '-', '-', '-', '-', '-' },
+			{ '-', '-', '-', '-', '-', '-', '-', '-' },
+			{ 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+			{ 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' } };
+		Uppercase pieces are black, lowercase pieces are white
+		K = King, Q = Queen, R = Rook, N = Knight, B = Bishop, P = Pawn, - = empty space */
+		int col = 0;
+		foreach (Piece piece in board) {
+			Console.Write("|" + piece);
+			col++;
+			if (col == dim) {
+				Console.WriteLine("|");
+				col = 0;
+			}
+		}
+		Console.WriteLine();
+	}
+	//Tester method
+	public static void Main(string[] args) {
+		//csc /out:Chess.exe Board.cs Pieces.cs
+		Board b = new Board();
+		b.Print();
+		b.Move(6, 0, 4, 0);
+		b.Move(1, 5, 2, 5);
+		b.Print();
+	}
+}
---
+++ Back-End/Pieces.cs
@@ -0,0 +1,131 @@
+﻿namespace Pieces {
+	public enum Color {
+		White, Black, Empty
+	}
+	public abstract class Piece {
+		protected Color c;
+		protected int id;
+		//Returns if the piece is controlled by White or Black
+		public Color GetColor() {
+			return c;
+		}
+		//Returns whether the piece is actually an empty square
+		public bool IsEmpty() {
+			return c == Color.Empty;
+		}
+		//Returns the piece ID
+		public int getID() {
+			return id;
+		}
+		//Returns the movement range of the piece
+		public abstract int GetMovement();
+		public abstract override string ToString();
+	}
+	public class Pawn : Piece {
+		public Pawn(Color c, int id) {
+			this.c = c;
+			this.id = id;
+		}
+		public override int GetMovement() {
+			return 1;
+		}
+		public override string ToString() {
+			if (c ==  Color.White) {
+				return "p" + id;
+			} else {
+				return "P" + id;
+			}
+		}
+	}
+	public class Bishop : Piece {
+		public Bishop(Color c, int id) {
+			this.c = c;
+			this.id = id;
+		}
+		public override int GetMovement() {
+			return 2;
+		}
+		public override string ToString() {
+			if (c == Color.White) {
+				return "b" + id;
+			} else {
+				return "B" + id;
+			}
+		}
+	}
+	public class Knight : Piece {
+		public Knight(Color c, int id) {
+			this.c = c;
+			this.id = id;
+		}
+		public override int GetMovement() {
+			return 4;
+		}
+		public override string ToString() {
+			if (c == Color.White) {
+				return "n" + id;
+			} else {
+				return "N" + id;
+			}
+		}
+	}
+	public class Rook : Piece {
+		public Rook(Color c, int id) {
+			this.c = c;
+			this.id = id;
+		}
+		public override int GetMovement() {
+			return 2;
+		}
+		public override string ToString() {
+			if (c == Color.White) {
+				return "r" + id;
+			} else {
+				return "R" + id;
+			}
+		}
+	}
+	public class Queen : Piece {
+		public Queen(Color c) {
+			this.c = c;
+			id = 0;//only 1 Queen
+		}
+		public override int GetMovement() {
+			return 3;
+		}
+		public override string ToString() {
+			if (c == Color.White) {
+				return "q" + id;
+			} else {
+				return "Q" + id;
+			}
+		}
+	}
+	public class King : Piece {
+		public King(Color c) {
+			this.c = c;
+			id = 0;//only 1 King
+		}
+		public override int GetMovement() {
+			return 3;
+		}
+		public override string ToString() {
+			if (c == Color.White) {
+				return "k" + id;
+			} else {
+				return "K" + id;
+			}
+		}
+	}
+	public class EmptySquare : Piece {
+		public EmptySquare() {
+			this.c = Color.Empty;
+		}
+		public override int GetMovement() {
+			return 0;
+		}
+		public override string ToString() {
+			return "--";
+		}
+	}
+}
