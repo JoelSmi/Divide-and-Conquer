5b8752100090796a2a27f587de271d3064444bd8
Joel Smith <71782153+JoelSmi@users.noreply.github.com>
2022-01-28 13:46:16 -0500

    Added position checking and reconsolidated Pieces
    For better access to information, I recombined all Piece classes into Piece.cs. Also added Color string variable to Pieces and the current position for each piece object.
    Added a position checking function for ensuring the piece object is accurately storing its current position

--- Execution Layer/Board.cs
+++ Execution Layer/Board.cs
@@ -7,9 +7,9 @@ namespace GameBoard
     public class Board
     {
         //Matricies storing the current game board and the white and black pieces being used
-        private string[,] GameBoard = new string[8, 8];
-        private Piece[,] WhiteBoard = new Piece[2, 8];
-        private Piece[,] BlackBoard = new Piece[2, 8];
+        public string[,] GameBoard { get; private set; } = new string[8, 8];
+        public Piece[,] WhiteBoard { get; private set; } = new Piece[2, 8];
+        public Piece[,] BlackBoard { get; private set; } = new Piece[2, 8];
 
         //bool value to track turn control
         protected bool isWhite;
@@ -41,28 +41,65 @@ namespace GameBoard
             for (int i = 0; i < this.GameBoard.GetLength(0); i++)
             {
                 //Black piece initialization for game board order for chess
-                this.GameBoard[0, i] = this.BlackBoard[1, i].getId();
-                this.GameBoard[1, i] = this.BlackBoard[0, i].getId();
+                this.GameBoard[0, i] = this.BlackBoard[1, i].id;
+                this.BlackBoard[1, i].currPos = new int[] { 0, i };
+                this.GameBoard[1, i] = this.BlackBoard[0, i].id;
+                this.BlackBoard[0, i].currPos = new int[]{ 1,i};
 
                 //White piece initialization for game board order for chess
-                this.GameBoard[6, i] = this.WhiteBoard[0, i].getId();
-                this.GameBoard[7, i] = this.WhiteBoard[1, i].getId();
+                this.GameBoard[6, i] = this.WhiteBoard[0, i].id;
+                this.WhiteBoard[0, i].currPos = new int[] { 6, i };
+                this.GameBoard[7, i] = this.WhiteBoard[1, i].id;
+                this.WhiteBoard[1, i].currPos = new int[] { 7, i };
             }
-            
+
             //Restart match starting with White taking the first turn
             isWhite = true;
+        }
+
+        public void checkBoardPos(Piece currPiece)
+        {
+            int[] Position = new int[2];
+
+            if (!this.GameBoard[currPiece.currPos[0], currPiece.currPos[1]].Equals(currPiece.id))
+            {
+                for (int x = 0; x < this.GameBoard.GetLength(0); x++)
+                {
+                    for (int y = 0; y < this.GameBoard.GetLength(1); y++)
+                    {
+                        if (this.GameBoard[x, y].Equals(currPiece.id))
+                        {
+                            Position = new int[] { x, y };
+                            currPiece.currPos = Position; 
+                        }
+                    }
+                }
+            }
         }
         
         public void updateBoard(int[] currPosition, int[] dest, Piece currPiece)
         {
             this.GameBoard[currPosition[0], currPosition[1]] = "e";
-            this.GameBoard[dest[0], dest[1]] = currPiece.getId();
+            this.GameBoard[dest[0], dest[1]] = currPiece.id;
+            currPiece.currPos = dest;
         }
         
-        public void takeAction()
+        public void takeAction(char ActionType, Piece currPiece, int[] dest)
         {
             
+            ///checkBoardPos(Piece);
+            ///if(ActionType == 'M'){
+            ///     Action.moveAction(currPiece, dest)
+            ///     
+            /// }elif(ActionType == 'A'){
+            ///     Action.attackAction(currPiece, dest)
+            ///     
+            /// }
+            
+            //Call Action.cs file for checking validity of action
+            //Once confirmed call update to perform action save
         }
+
 
         public void endTurn()
         {
--- Execution Layer/Piece.cs
+++ Execution Layer/Piece.cs
@@ -1,4 +1,3 @@
-
 namespace Pieces
 {
     public abstract class Piece
@@ -8,54 +7,148 @@ namespace Pieces
             Movement type denoted by a single character defines how the peice moves/in what direction it can move
                 (i.e. F means forward in the derection of opposing side, S means Square centered at the current position of the piece)
         */
-        protected short movement = 0;
-        protected char movementType = 'N';
+        public short movement { get; protected set; } = 0;
+        public char movementType { get; protected set; } = 'N';
 
         /*
             * DefenseProb stores the number of ways to succeed against the current piece (i.e. 2 means that only 5 and 6 
             * are the only numbers that the attacker can roll for success against the Bishop) the array positions for the attacker identity
             * follows: {Pawn, Rook, Bishop, Knight, Queen, King}
         */
-        protected short[] defenseProb = new short[6];
+        public short[] defenseProb { get; protected set; } = new short[6];
 
         /*
             Attack stores the number of spaces the current piece type has to be able to attack, 1 means only adjacent spaces
             Attack type denoted by a single character defines how the peice attacks/in what direction it can attack
                 (i.e. F means forward in the derection of opposing side, S means Square centered at the current position of the piece, R means ranged)
         */
-        protected short attack = 0;
-        protected char attackType = 'N';
+        public short attack { get; protected set; } = 0;
+        public char attackType { get; protected set; } = 'N';
 
-        protected string id = "";
+        /*
+            The color indicates which team the peice belongs to while the id
+            is how the piece is represnted on the board
+        */
+        public string color { get; protected set; } = "";
+        public string id { get; protected set; } = "";
 
-        public short getMovement()
+        //boolean value to determine if a peice has been captured or not
+        public bool isCaptured { get; protected set; } = false;
+        //Storage of the current position of the peice in the format of {x,y}, or {row,column}
+        public int[] currPos { get; set; } = new int[2];
+    }
+
+    public class Pawn : Piece
+    {
+        public Pawn(string id, string Color)
         {
-            return this.movement;
+            this.movement = 1;
+            this.movementType = 'F';
+            this.defenseProb = new short[] { 3, 2, 4, 5, 5, 6 };
+
+            this.attack = 1;
+            this.attackType = 'F';
+
+            this.color = Color;
+            this.id = id;
         }
+    }
 
-        public char getMovementType()
+    public class Rook : Piece
+    {
+
+        public Rook(string id, string Color)
         {
-            return this.movementType;
+            this.movement = 2;
+            this.movementType = 'L';
+            this.defenseProb = new short[] { 1, 2, 2, 2, 2, 2 };
+
+            this.attack = 3;
+            this.attackType = 'R';
+
+            this.color = Color;
+            this.id = id;
         }
+    }
 
-        public short[] getDefenseProb()
+    public class Knight : Piece
+    {
+        public Knight(string id, string Color)
         {
-            return this.defenseProb;
+            this.movement = 4;
+            this.movementType = 'S';
+            this.defenseProb = new short[] { 1, 3, 2, 2, 3, 3 };
+
+            this.attack = 1;
+            this.attackType = 'S';
+
+            this.color = Color;
+            this.id = id;
         }
+    }
 
-        public short getAttack()
+    ///Both the Bishop and King have two addition variables 
+    ///called delegates and action count
+    public class Bishop : Piece
+    {
+        //Stores the ids' of the piece currently under the command of the bishop
+        public string[] Delegates { get; protected set; } = new string[6];
+        //Stores the maximum numbe of actions this peice can perform or command
+        public int actionCount { get; protected set; } = 0;
+
+        public Bishop(string id, string Color)
         {
-            return this.attack;
+            this.movement = 2;
+            this.movementType = 'L';
+
+            this.defenseProb = new short[] { 2, 2, 3, 2, 2, 3 };
+
+            this.attack = 1;
+            this.attackType = 'S';
+
+            this.actionCount = 0;
+
+            this.color = Color;
+            this.id = id;
         }
+    }
 
-        public char getAttackType()
+    public class Queen : Piece
+    {
+        public Queen(string id, string Color)
         {
-            return this.attackType;
+            this.movement = 3;
+            this.movementType = 'S';
+            this.defenseProb = new short[] { 1, 3, 2, 2, 3, 3 };
+
+            this.attack = 1;
+            this.attackType = 'S';
+
+            this.color = Color;
+            this.id = id;
         }
+    }
 
-        public string getId()
+    public class King : Piece
+    {
+        //Stores the ids' of the piece currently under the command of the bishop\
+        public string[] Delegates { get; protected set; } = new string[16];
+        //Stores the maximum numbe of actions this peice can perform or command
+        public int actionCount { get; protected set; } = 0;
+
+        public King(string id, string Color)
         {
-            return this.id;
+            this.movement = 4;
+            this.movementType = 'S';
+            this.defenseProb = new short[] { 1, 3, 2, 2, 3, 3 };
+
+            this.attack = 1;
+            this.attackType = 'S';
+
+            this.actionCount = 6;
+
+            this.color = Color;
+            this.id = id;
         }
     }
 }
