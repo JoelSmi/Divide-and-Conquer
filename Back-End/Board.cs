3b6c1d9a4922ff08a6e55ef2b6b4fb7af47e568c
malibak4 <malibak4@students.kennesaw.edu>
2022-02-02 21:41:30 -0500

    Updated functions for getting legal moves and legal attacks for pieces, as well as adding auxiliary functions for them

--- Back-End/Board.cs
+++ Back-End/Board.cs
@@ -20,18 +20,24 @@ public class Board {
 			{ new Rook(Color.White, 0), new Knight(Color.White, 0), new Bishop(Color.White, 0), new Queen(Color.White),
 			new King(Color.White), new Bishop(Color.White, 1), new Knight(Color.White, 1), new Rook(Color.White, 1) }};
 		dim = 8;
+		this.UpdateAllLegalMoves();
 	}
-	//Moves a piece at the coordinates [PieceX, PieceY] to the space on the board with the coordinates [DestinationX, DestinationY]
-	public void Move(int PieceX, int PieceY, int DestinationX, int DestinationY) {
-		board[DestinationX, DestinationY] = board[PieceX, PieceY];
-		board[PieceX, PieceY] = e;//Target's previous space is now empty
+	/**Moves a piece at the coordinates [PieceX, PieceY] to the space on the board with the coordinates [DestinationX, DestinationY]
+	 * Precondition: Both coordinate pairs are in bounds for this board and the destination space is a legal move */
+	public void Move(int PieceRow, int PieceCol, int DestinationRow, int DestinationCol) {
+		board[DestinationRow, DestinationCol] = board[PieceRow, PieceCol];
+		board[PieceRow, PieceCol] = e;//Target's previous space is now empty
+		Console.WriteLine("Moved piece " + board[DestinationRow, DestinationCol] 
+			+ " to " + GetNotation(DestinationRow, DestinationCol));
+		this.UpdateAllLegalMoves();
 	}
-	public Piece[,] getBoard() {
+	public Piece[,] GetBoard() {
 		return board;
 	}
 	public int width() {
 		return dim;
 	}
+	//TODO: Add rank and file labels
 	public void Print() {
 		/* Visualization of how the default board is arranged:
 		char[,] textBoard = new char[8, 8] {
@@ -56,20 +62,81 @@ public class Board {
 		}
 		Console.WriteLine();
 	}
+	//Update legal moves and attacks for all pieces on the board
+	public void UpdateAllLegalMoves() {
+		for (int col = 0; col < dim; col++) {
+			for (int row = 0; row < dim; row++) {
+				Piece p = board[row, col];
+				if (p.GetType() != typeof(EmptySquare)) {
+					//Clear the list of legal attacks and prepare to update in the recursive helper method
+					p.SetLegalAttacks(new HashSet<int[]>());
+					//Call the recursive helper method to traverse each legal space
+					p.UpdateLegalActions(this, row, col);
+				}
+			}
+		}
+	}
+	//Returns whether a coordinate pair maps to an existing space on this board
+	public bool IsInBounds(int row, int col) {
+		return row < dim && row >= 0 && col < dim && col >= 0;
+	}
+	/* Returns the squares adjacent to the starting square (row, col)
+	 * as a map of legal directions and their squares' coordinate pair values */
+	public Dictionary<Direction, int[]> GetAdjacentSquares(int row, int col) {
+		Dictionary<Direction, int[]> directions = new Dictionary<Direction, int[]>();
+		directions[Direction.North] = new int[] { row - 1, col };
+		directions[Direction.South] = new int[] { row + 1, col };
+		directions[Direction.West] = new int[] { row, col - 1 };
+		directions[Direction.East] = new int[] { row, col + 1 };
+		directions[Direction.Northwest] = new int[] { row - 1, col - 1 };
+		directions[Direction.Northeast] = new int[] { row - 1, col + 1 };
+		directions[Direction.Southwest] = new int[] { row + 1, col - 1 };
+		directions[Direction.Southeast] = new int[] { row + 1, col + 1 };
+		//Filter out squares that are out of bounds
+		HashSet<Direction> keys = new HashSet<Direction>(directions.Keys);
+		foreach (Direction dir in keys) {
+			if (!IsInBounds(directions[dir][0], directions[dir][1])) {
+				directions.Remove(dir);
+			}
+		}
+		return directions;
+	}
+	//Returns a set of the squares within [range] squares of the square at (row, col)
+	public HashSet<int[]> GetSquaresInRange(int row, int col, int range) {
+		HashSet<int[]> squares = new HashSet<int[]>();
+		return GetSquaresInRange(row, col, range, squares);
+		
+	}
+	//Recursive helper method
+	private HashSet<int[]> GetSquaresInRange(int row, int col, int range, HashSet<int[]> squares) {
+		if (range <= 0) {
+			return squares;
+		}
+		HashSet<int[]> adjacentSquares = new HashSet<int[]>(GetAdjacentSquares(row, col).Values);
+		foreach (int[] adjacentSquare in adjacentSquares) { 
+			if (!SetContainsSquare(squares, adjacentSquare)) {
+				squares.Add(adjacentSquare);
+			}
+		}
+		foreach (int[] adjacentSquare in adjacentSquares) {
+			squares = new HashSet<int[]>(GetSquaresInRange(adjacentSquare[0], adjacentSquare[1], range - 1, squares));
+		}
+		return squares;
+	}
 	//Prints the board along with the legal squares (not attacks) for the Piece on a square
 	//Each legal empty square a piece can visit is marked as XX
 	public void PrintLegalSquares(int[] square) {
 		//First print the piece and its legal squares as coordinate pairs
 		Piece p = board[square[0], square[1]];
 		Console.WriteLine("Legal squares for " + p + ": ");
-		foreach (int[] move in p.GetLegalMoves()) {
-			Console.WriteLine(move[0] + ", " + move[1]);
-		}
+		/*foreach (int[] move in p.GetLegalMoves()) {
+			Console.WriteLine("(" + move[0] + ", " + move[1] + ")");
+		}*/
 		//Printing the board
 		HashSet<int[]> legalSquares = p.GetLegalMoves();
 		for (int col = 0; col < dim; col++) {
 			for (int row = 0; row < dim; row++) {
-				if (BoardFunctions.SetContainsSquare(legalSquares, new int[] { col, row })) {
+				if (SetContainsSquare(legalSquares, new int[] { col, row })) {
 					Console.Write("|XX"); //An empty space this piece can move to
 				} else {
 					Console.Write("|" + board[col, row]);
@@ -79,20 +146,51 @@ public class Board {
 		}
 		Console.WriteLine();
 	}
+	//Prints the legal attacks for the Piece on a square
+	public void PrintLegalAttacks(int[] square) {
+		Piece p = board[square[0], square[1]];
+		Console.Write("Legal attack targets for " + p + ": ");
+		foreach (int[] move in p.GetLegalAttacks()) {
+			Console.Write(board[move[0], move[1]] + " ");
+		}
+		Console.WriteLine();
+	}
+	//Returns whether a set of squares contains a chosen square
+	public static bool SetContainsSquare(HashSet<int[]> hs, int[] square) {
+		foreach (int[] setSquare in hs) {
+			if (setSquare[0] == square[0] && setSquare[1] == square[1]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	/** Converts a square coordinate pair to chess notation (e.g. G4)
+	 * Precondition: square is in bounds */
+	public static string GetNotation(int squareRow, int squareCol) {
+		string row = (squareRow + 1).ToString();
+		char column = (char) (65 + squareCol);
+		return column + row;
+	}
 	//Tester method
 	public static void Main(string[] args) {
 		//csc /out:Chess.exe Board.cs Pieces.cs BoardFunctions.cs
 		Board b = new Board();
 		b.Print();
-		BoardFunctions.UpdateLegalMoves(b);
 		b.PrintLegalSquares(new int[] { 6, 0 });//p0
 
-		b.Move(6, 0, 4, 0);
-		b.Move(1, 5, 2, 5);
+		b.Move(6, 5, 5, 5);//p5 up 1 to F6
+		b.Move(1, 5, 2, 5);//P5 down 1 to F3
 		b.Print();
-		BoardFunctions.UpdateLegalMoves(b);
+
 		b.PrintLegalSquares(new int[] { 0, 5 });//B1
 		b.PrintLegalSquares(new int[] { 0, 6 });//N1
+		b.PrintLegalSquares(new int[] { 7, 5 });//b1
+		b.PrintLegalSquares(new int[] { 7, 6 });//n1
+		b.PrintLegalAttacks(new int[] { 7, 6 });//n1
+
+		b.Move(0, 6, 4, 7);//N1 to h5
+		b.Print();
+		b.PrintLegalAttacks(new int[] { 7, 7 });//r1
 
 		Console.WriteLine("Minimum roll for Knight to capture Queen:" + Piece.getMinimumRoll(new Knight(Color.White, 0), new Queen(Color.Black)));
 		Console.WriteLine("Minimum roll for King to capture Pawn:" + Piece.getMinimumRoll(new King(Color.White), new Pawn(Color.Black, 0)));
--- Back-End/Pieces.cs
+++ Back-End/Pieces.cs
@@ -4,9 +4,15 @@ namespace Pieces {
 	public enum Color {
 		White, Black, Empty
 	}
+	public enum MovementType {
+		Free, Linear, None
+	}
+	public enum Direction {
+		North, Northwest, Northeast, South, Southwest, Southeast, West, East, None
+	}
 	public abstract class Piece {
 		//Minimum rolls for each piece to capture another piece
-		static int[,] attackerTable = {
+		protected static int[,] attackerTable = {
 		{4, 4, 4, 4, 5, 1},
 		{4, 4, 4, 4, 5, 2},
 		{5, 5, 5, 5, 5, 2},
@@ -25,21 +31,32 @@ namespace Pieces {
 				return attackerTable[attackerIndex, defenderIndex];
 			}
 		}
-		protected Color c;
-		protected int id;
-		protected HashSet<int[]> legalMoves;//Movement only
-		protected HashSet<int[]> legalAttacks;
+		protected Color color;
+		protected int id, movement;
+		protected MovementType movementType;
+		protected HashSet<int[]> legalMoves, legalAttacks;
+		protected HashSet<Direction> omni = new HashSet<Direction>(new Direction[] { Direction.North, Direction.Northwest, Direction.Northeast,
+			Direction.South, Direction.Southwest, Direction.Southeast, Direction.West, Direction.East });
+		protected HashSet<Direction> legalDirections;
 		//Returns if the piece is controlled by White or Black
 		public Color GetColor() {
-			return c;
+			return color;
 		}
 		//Returns whether the piece is actually an empty square
 		public bool IsEmpty() {
-			return c == Color.Empty;
+			return color == Color.Empty;
 		}
 		//Returns the piece ID
 		public int GetID() {
 			return id;
+		}
+		//Returns the movement range of the piece
+		public int GetMovement() {
+			return movement;
+		}
+		//Gets the movement type of the piece (whether it can turn during a move or not)
+		public MovementType GetMovementType() {
+			return movementType;
 		}
 		//Gets the set of legal spaces for this piece to move to by their coordinate pairs
 		public HashSet<int[]> GetLegalMoves() {
@@ -49,26 +66,118 @@ namespace Pieces {
 		public HashSet<int[]> GetLegalAttacks() {
 			return legalAttacks;
 		}
-		public void SetLegalMoves(HashSet<int[]> legalMoves) {
-			this.legalMoves = legalMoves;
+		//Update the legal moves and attacks for this piece based on the board and its position
+		public void UpdateLegalActions(Board b, int row, int col) {
+			//Update legal moves using recursive helper method
+			this.legalMoves = UpdateLegalMoves(b, row, col, movement, new HashSet<int[]>(), Direction.None);
+			//Update legal attacks
+			legalAttacks.Clear();
+			if (this.GetType() == typeof(Rook)) {
+				//Allow rook to attack any enemy piece within 3 squares
+				foreach (int[] square in b.GetSquaresInRange(row, col, 3)) {
+					if (b.GetBoard()[square[0], square[1]].GetColor() != Color.Empty
+							&& b.GetBoard()[square[0], square[1]].GetColor() != color) {
+						legalAttacks.Add(square);
+					}
+				}
+			} else {
+				//Add adjacent spaces containing enemy pieces to the list of legal attacks
+				foreach (int[] square in b.GetAdjacentSquares(row, col).Values) {
+					if (b.GetBoard()[square[0], square[1]].GetColor() != Color.Empty
+							&& b.GetBoard()[square[0], square[1]].GetColor() != color
+							&& !Board.SetContainsSquare(legalAttacks, square)) {
+						legalAttacks.Add(square);
+					}
+				}
+				//Allow knight to also attack any piece adjacent to one of its legal squares
+				if (this.GetType() == typeof(Knight)) {
+					foreach (int[] legalMove in legalMoves) { 
+						foreach (int[] square in b.GetAdjacentSquares(legalMove[0], legalMove[1]).Values) {
+							if (b.GetBoard()[square[0], square[1]].GetColor() != Color.Empty
+									&& b.GetBoard()[square[0], square[1]].GetColor() != color
+									&& !Board.SetContainsSquare(legalAttacks, square)) {
+								legalAttacks.Add(square);
+							}
+						}
+					}
+				}
+			}
 		}
 		public void SetLegalAttacks(HashSet<int[]> legalAttacks) {
 			this.legalAttacks = legalAttacks;
 		}
-		//Returns the movement range of the piece
-		public abstract int GetMovement();
+		//Gets the set of legal directions this piece can move in
+		public HashSet<Direction> GetLegalDirections() {
+			return legalDirections;
+		}
+		/**Recursive helper function for pathing all legal moves for a piece
+		* TODO: Implement Rook and Knight legal attacks
+		* Precondition: Piece p is not an EmptySquare */
+		private HashSet<int[]> UpdateLegalMoves(Board b, int row, int col, int remainingMov, HashSet<int[]> legalMoves, Direction movementDir) {
+			if (remainingMov <= 0) {
+				//Piece has no movement remaining, so return the compiled set of legal moves
+				return legalMoves;
+			}
+			//Create the coordinate pairs of the 8 adjacent squares
+			Dictionary<Direction, int[]> directions = b.GetAdjacentSquares(row, col);
+			//Create combinations of these squares according to the legal movement directions for each piece
+			if (movementDir != Direction.None && movementType == MovementType.Linear) {
+				//Must continue moving in the direction we were moving
+				foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+					if (dir != movementDir) {
+						directions.Remove(dir);
+					}
+				}
+			} else {
+				//Make the list conform to the set of legal directions for this piece
+				foreach (Direction dir in Enum.GetValues(typeof(Direction))) {
+					if (!legalDirections.Contains(dir)) {
+						directions.Remove(dir);
+					}
+				}
+			}
+			//Filter out spaces that are out of bounds or occupied by an ally piece
+			HashSet<Direction> directionKeys = new HashSet<Direction>(directions.Keys);
+			foreach (Direction dir in directionKeys) {
+				int[] square = directions[dir];
+				if (!b.IsInBounds(square[0], square[1]) || Board.SetContainsSquare(legalMoves, square)
+						|| b.GetBoard()[square[0], square[1]].GetColor() == color) {
+					directions.Remove(dir);
+				} else if (b.GetBoard()[square[0], square[1]].GetColor() != Color.Empty) {
+					directions.Remove(dir);
+					if (remainingMov == movement) {//Piece is adjacent to an enemy piece
+						legalAttacks.Add(square);
+					}
+				} else {
+					//A legal empty square which we have not yet traversed has been found
+					legalMoves.Add(square);
+				}
+			}
+			foreach (Direction dir in directions.Keys) {
+				int[] square = directions[dir];
+				//Update the list of legal moves by traversing to each square in the queue and finding the legal moves given that square
+				legalMoves = new HashSet<int[]>(UpdateLegalMoves(b, square[0], square[1], remainingMov - 1, legalMoves, dir));
+			}
+			return legalMoves;
+		}
 		public abstract override string ToString();
 	}
 	public class Pawn : Piece {
 		public Pawn(Color c, int id) {
-			this.c = c;
+			this.color = c;
 			this.id = id;
-		}
-		public override int GetMovement() {
-			return 1;
+			movement = 1;
+			movementType = MovementType.Linear;
+			if (color == Color.White) {
+				legalDirections = new HashSet<Direction>(new Direction[] { Direction.North, Direction.Northwest, Direction.Northeast });
+			} else if (color == Color.Black) {
+				legalDirections = new HashSet<Direction>(new Direction[] { Direction.South, Direction.Southwest, Direction.Southeast });
+			} else {//This shouldn't happen
+				legalDirections = new HashSet<Direction>();
+			}
 		}
 		public override string ToString() {
-			if (c ==  Color.White) {
+			if (color ==  Color.White) {
 				return "p" + id;
 			} else {
 				return "P" + id;
@@ -77,14 +186,14 @@ namespace Pieces {
 	}
 	public class Bishop : Piece {
 		public Bishop(Color c, int id) {
-			this.c = c;
+			this.color = c;
 			this.id = id;
-		}
-		public override int GetMovement() {
-			return 2;
+			movement = 2;
+			movementType = MovementType.Linear;
+			legalDirections = omni;
 		}
 		public override string ToString() {
-			if (c == Color.White) {
+			if (color == Color.White) {
 				return "b" + id;
 			} else {
 				return "B" + id;
@@ -93,14 +202,14 @@ namespace Pieces {
 	}
 	public class Knight : Piece {
 		public Knight(Color c, int id) {
-			this.c = c;
+			this.color = c;
 			this.id = id;
-		}
-		public override int GetMovement() {
-			return 4;
+			movement = 4;
+			movementType = MovementType.Free;
+			legalDirections = omni;
 		}
 		public override string ToString() {
-			if (c == Color.White) {
+			if (color == Color.White) {
 				return "n" + id;
 			} else {
 				return "N" + id;
@@ -109,14 +218,14 @@ namespace Pieces {
 	}
 	public class Rook : Piece {
 		public Rook(Color c, int id) {
-			this.c = c;
+			this.color = c;
 			this.id = id;
-		}
-		public override int GetMovement() {
-			return 2;
+			movement = 2;
+			movementType = MovementType.Linear;
+			legalDirections = omni;
 		}
 		public override string ToString() {
-			if (c == Color.White) {
+			if (color == Color.White) {
 				return "r" + id;
 			} else {
 				return "R" + id;
@@ -125,14 +234,14 @@ namespace Pieces {
 	}
 	public class Queen : Piece {
 		public Queen(Color c) {
-			this.c = c;
+			this.color = c;
 			id = 0;//only 1 Queen
-		}
-		public override int GetMovement() {
-			return 3;
+			movement = 3;
+			movementType = MovementType.Free;
+			legalDirections = omni;
 		}
 		public override string ToString() {
-			if (c == Color.White) {
+			if (color == Color.White) {
 				return "q" + id;
 			} else {
 				return "Q" + id;
@@ -141,14 +250,14 @@ namespace Pieces {
 	}
 	public class King : Piece {
 		public King(Color c) {
-			this.c = c;
+			this.color = c;
 			id = 0;//only 1 King
-		}
-		public override int GetMovement() {
-			return 3;
+			movement = 3;
+			movementType = MovementType.Free;
+			legalDirections = omni;
 		}
 		public override string ToString() {
-			if (c == Color.White) {
+			if (color == Color.White) {
 				return "k" + id;
 			} else {
 				return "K" + id;
@@ -157,13 +266,13 @@ namespace Pieces {
 	}
 	public class EmptySquare : Piece {
 		public EmptySquare() {
-			this.c = Color.Empty;
-		}
-		public override int GetMovement() {
-			return 0;
+			this.color = Color.Empty;
+			movement = 0;
+			movementType = MovementType.None;
+			legalDirections = new HashSet<Direction>();
 		}
 		public override string ToString() {
-			return "--";
+			return "  ";
 		}
 	}
 }
