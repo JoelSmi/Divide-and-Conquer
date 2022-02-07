fa3ad0fdc48445b3d803469347de1eecbd00914c
fchsballa3 <fchsballa3@gmail.com>
2022-02-07 14:56:47 -0500

    Adding Execution Layer Changes to UI

--- Front_End/AiChessSprint1/Assets/Scripts/ExecutionLayer/Board.cs
+++ Front_End/AiChessSprint1/Assets/Scripts/ExecutionLayer/Board.cs
@@ -6,15 +6,27 @@ namespace GameBoard
 {
     public class Board
     {
+        /*
+         * Board On Initialization:
+         * (id representation)
+         * |R0 N0 B0 Q0 K0 B1 N1 R1|
+         * |P0 P1 P2 P3 P4 P5 P6 P7|
+         * |e  e  e  e  e  e  e  e |
+         * |e  e  e  e  e  e  e  e |
+         * |e  e  e  e  e  e  e  e |
+         * |e  e  e  e  e  e  e  e |
+         * |p0 p1 p2 p3 p4 p5 p6 p7|
+         * |r0 n0 b0 q0 k0 b1 n1 r1|
+         */
+
         //Matricies storing the current game board and the white and black pieces being used
         public Piece[,] GameBoard { get; private set; } = new Piece[8, 8];
         public Piece[,] WhiteBoard { get; private set; } = new Piece[2, 8];
         public Piece[,] BlackBoard { get; private set; } = new Piece[2, 8];
-        private Piece blank = new Blank("e", "");
-
+        public Piece Blank = new Empty("e","N");
+        private const int MaxActionCount = 6;
         //bool value to track turn control
-        protected bool isWhite;
-        public bool isUpdated { get; set; } = false;
+        public bool isWhite { get; protected set; } = true;
 
         public Board(Piece[,] initialWhite, Piece[,] initialBlack)
         { 
@@ -30,14 +42,14 @@ namespace GameBoard
         {
             //initial state of game board before peice setup
             this.GameBoard = new Piece[,] {
-                {blank,blank,blank,blank,blank,blank,blank,blank},
-                {blank,blank,blank,blank,blank,blank,blank,blank},
-                {blank,blank,blank,blank,blank,blank,blank,blank},
-                {blank,blank,blank,blank,blank,blank,blank,blank},
-                {blank,blank,blank,blank,blank,blank,blank,blank},
-                {blank,blank,blank,blank,blank,blank,blank,blank},
-                {blank,blank,blank,blank,blank,blank,blank,blank},
-                {blank,blank,blank,blank,blank,blank,blank,blank}
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank},
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank},
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank},
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank},
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank},
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank},
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank},
+                {Blank,Blank,Blank,Blank,Blank,Blank,Blank,Blank}
             };
 
             for (int i = 0; i < this.GameBoard.GetLength(0); i++)
@@ -63,13 +75,13 @@ namespace GameBoard
         {
             int[] Position = new int[2];
 
-            if (!this.GameBoard[currPiece.currPos[0], currPiece.currPos[1]].Equals(currPiece))
+            if (!this.GameBoard[currPiece.currPos[0], currPiece.currPos[1]].Equals(currPiece.id))
             {
                 for (int x = 0; x < this.GameBoard.GetLength(0); x++)
                 {
                     for (int y = 0; y < this.GameBoard.GetLength(1); y++)
                     {
-                        if (this.GameBoard[x, y].Equals(currPiece))
+                        if (this.GameBoard[x, y].Equals(currPiece.id))
                         {
                             Position = new int[] { x, y };
                             currPiece.currPos = Position; 
@@ -79,29 +91,49 @@ namespace GameBoard
             }
         }
         
-        public void updateBoard(int[] currPosition, int[] dest, Piece currPiece)
+        public void updateBoard(Piece currPiece, int[] currPosition, int[] dest)
         {
-            this.GameBoard[currPosition[0], currPosition[1]] = blank;
+            this.GameBoard[currPosition[0], currPosition[1]] = this.Blank;
             this.GameBoard[dest[0], dest[1]] = currPiece;
             currPiece.currPos = dest;
+        }
+
+        public char checkActionType(int[] currPos, int[] dest)
+        {
+            if (currPos[0] == dest[0] && currPos[1] == dest[1])
+                return 'N';
+            else if (this.GameBoard[dest[0], dest[1]] == Blank)
+                return 'M';
+            else if (!this.GameBoard[currPos[0], currPos[1]].color.Equals(this.GameBoard[dest[0], dest[1]].color))
+                return 'A';
+            else
+                return 'I';
         }
         
         public void takeAction(char ActionType, Piece currPiece, int[] dest)
         {
-            
-            ///checkBoardPos(Piece);
-            ///if(ActionType == 'M'){
-            ///     Action.moveAction(currPiece, dest)
-            ///     
-            /// }elif(ActionType == 'A'){
-            ///     Action.attackAction(currPiece, dest)
-            ///     
-            /// }
-            
-            //Call Action.cs file for checking validity of action
-            //Once confirmed call update to perform action save
-        }
 
+            int temp = 0;
+            int ActionCount = 0;
+            switch (ActionType) {
+                case 'M':
+                    temp = Actions.Action.moveAction(this.GameBoard, currPiece, currPiece.currPos, dest);
+                    if(temp > 0 && ActionCount+temp < MaxActionCount)
+                    {
+                        updateBoard(currPiece, currPiece.currPos, dest);
+                        endTurn();
+                    }
+                    break;
+                case 'A':
+                    temp = Actions.Action.attackAction(this.GameBoard, currPiece, currPiece.currPos, dest);
+                    if (temp > 0 && ActionCount + temp < MaxActionCount)
+                    {
+                        updateBoard(currPiece, currPiece.currPos, dest);
+                        endTurn();
+                    }
+                    break;
+            } 
+        }
 
         public void endTurn()
         {
