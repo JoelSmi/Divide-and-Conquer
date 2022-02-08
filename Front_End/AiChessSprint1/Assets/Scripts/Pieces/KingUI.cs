57de07f5acd0df21edaaf29d1b7e81c2283c5516
Hrhodus1 <hrhodus1@students.kennesaw.edu>
2022-02-08 17:03:56 -0500

    king and queen updated movement

--- Front_End/AiChessSprint1/Assets/Scripts/Pieces/KingUI.cs
+++ Front_End/AiChessSprint1/Assets/Scripts/Pieces/KingUI.cs
@@ -14,6 +14,40 @@ public class KingUI : BasePiece
         mMovement = new Vector3Int(3, 3, 3);
         GetComponent<Image>().sprite = Resources.Load<Sprite>("T_King");
     }
+    private bool MatchesState(int targetX, int targetY, CellState targetState)
+    {
+        CellState cellState = CellState.None;
+        cellState = mCurrentCell.mBoardUI.ValidateCell(targetX, targetY, this);
+        if (cellState == targetState)
+        {
+            mHighlightedCells.Add(mCurrentCell.mBoardUI.mAllCells[targetX, targetY]);
+            return true;
+        }
+
+        return false;
+    }
+
+    protected override void CheckPathing()
+    {
+        base.CheckPathing();
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+        if (MatchesState(currentX, currentY + 1, CellState.Free) || MatchesState(currentX, currentY + 1, CellState.Free))
+        {
+            MatchesState(currentX - 1, currentY + 2, CellState.Free);
+            MatchesState(currentX + 1, currentY + 2, CellState.Free);
+            MatchesState(currentX - 1, currentY - 2, CellState.Free);
+            MatchesState(currentX + 1, currentY - 2, CellState.Free);
+        }
+        if (MatchesState(currentX - 1, currentY, CellState.Free) || MatchesState(currentX + 1, currentY, CellState.Free))
+        {
+            MatchesState(currentX - 2, currentY + 1, CellState.Free);
+            MatchesState(currentX - 2, currentY - 1, CellState.Free);
+            MatchesState(currentX + 2, currentY + 1, CellState.Free);
+            MatchesState(currentX + 2, currentY - 1, CellState.Free);
+        }
+        
+    }
 
     //follows base Kill function and then set the mIsKingAlive object to false to reset the game
     public override void Kill()
--- Front_End/AiChessSprint1/Assets/Scripts/Pieces/QueenUI.cs
+++ Front_End/AiChessSprint1/Assets/Scripts/Pieces/QueenUI.cs
@@ -14,5 +14,41 @@ public class QueenUI : BasePiece
         mMovement = new Vector3Int(3, 3, 3);
         GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Queen");
     }
+    //checks if the state matches the state in the CheckPathing function if so it adds the move possibility to the MhighlightedCells
+    private bool MatchesState(int targetX, int targetY, CellState targetState)
+    {
+        CellState cellState = CellState.None;
+        cellState = mCurrentCell.mBoardUI.ValidateCell(targetX, targetY, this);
+        if (cellState == targetState)
+        {
+            mHighlightedCells.Add(mCurrentCell.mBoardUI.mAllCells[targetX, targetY]);
+            return true;
+        }
+
+        return false;
+    }
+
+    protected override void CheckPathing()
+    {
+        base.CheckPathing();
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+        if (MatchesState(currentX, currentY + 1, CellState.Free) || MatchesState(currentX, currentY + 1, CellState.Free))
+        {
+            MatchesState(currentX - 1, currentY + 2, CellState.Free);
+            MatchesState(currentX + 1, currentY + 2, CellState.Free);
+          
+        
+            MatchesState(currentX - 1, currentY - 2, CellState.Free);
+            MatchesState(currentX + 1, currentY - 2, CellState.Free);
+        }
+        if (MatchesState(currentX - 1, currentY, CellState.Free)|| MatchesState(currentX + 1, currentY, CellState.Free))
+        {
+            MatchesState(currentX - 2, currentY + 1, CellState.Free);
+            MatchesState(currentX - 2, currentY - 1, CellState.Free);
+            MatchesState(currentX + 2, currentY + 1, CellState.Free);
+            MatchesState(currentX + 2, currentY - 1, CellState.Free);
+        }
+    }   
 
 }
