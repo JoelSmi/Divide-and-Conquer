1d7e2ba80f9f07cc0ebac6f62e36a7f998f08366
BigMac1999 <91208688+BigMac1999@users.noreply.github.com>
2022-02-09 03:14:12 -0500

    Update AITesting.cs
    Added hardcoded boards for us to begin to test AI with
    Took out hard coded definitions of current commander, subordinates, and liveEnemyPlayers
    Added if statement to make sure we did not move() unless Action.isActing = true

--- Back-End/AITesting.cs
+++ Back-End/AITesting.cs
@@ -3,9 +3,7 @@ using System;
 namespace BishopAI1;
 
 class AITesting
-{
-
-    
+{    
     static void Main(string[] args){
 
         //At first we will always use the left bishop.
@@ -24,27 +22,52 @@ class AITesting
                                {"r0","n0","b0","q0","k0","b1","n1","r1"}
         }; 
 
+        String[,] boardArray1 = new String [8,8] {
+                               {"R0","N0","B0","Q0","K0","B1","N1","R1"},
+                               {"e","P1","P2","P3","P4","P5","P6","P7"},
+                               {"e","e","e","e","e","e","e","e"},
+                               {"e","e","e","e","e","e","e","e"},
+                               {"e","e","e","e","e","e","e","e"},
+                               {"P0","e","e","e","e","e","e","e"},
+                               {"p0","p1","p2","p3","p4","p5","p6","p7"},
+                               {"r0","n0","b0","q0","k0","b1","n1","r1"}
+        };
+
+        String[,] boardArray2 = new String [8,8] {
+                               {"R0","N0","B0","Q0","K0","B1","N1","R1"},
+                               {"e","e","e","P3","P4","P5","P6","P7"},
+                               {"e","e","e","e","e","e","e","e"},
+                               {"e","e","e","e","e","e","e","e"},
+                               {"e","e","e","e","e","e","e","e"},
+                               {"P0","e","P1","e","P2","e","e","e"},
+                               {"p0","p1","p2","p3","p4","p5","p6","p7"},
+                               {"r0","n0","b0","q0","k0","b1","n1","r1"}
+        }; 
+
         //Here we will need to be able to input the board from the middle layer, for now we will create a temp board.
         //Board b = new Board();
-        Board b = new Board(boardArray);
-        
+        //Board b = new Board(boardArray);
+        Board b = new Board(boardArray1);
+
         Console.WriteLine("Board Initialized");
         b.Print();
 
-        Piece currentCommander = b.GetPiece(0,2);
-        Piece[] subordinates = {b.GetPiece(1, 0), b.GetPiece(1, 1), b.GetPiece(1, 2), b.GetPiece(0, 1)};
-        Piece[] LiveEnemyPlayers =
-        {
-            b.GetPiece(6, 0), b.GetPiece(6,1), b.GetPiece(6,2), b.GetPiece(6,3),
-            b.GetPiece(6, 4), b.GetPiece(6,5), b.GetPiece(6,6), b.GetPiece(6,7),
-            b.GetPiece(7, 0), b.GetPiece(7,1), b.GetPiece(7,2), b.GetPiece(7,3),
-            b.GetPiece(7, 4), b.GetPiece(7,5), b.GetPiece(7,6), b.GetPiece(7,7),
-        };
+        Piece currentCommander = b.GetBishopCommander1();
+        Piece[] subordinates = b.GetSubordinates(currentCommander);
+        Piece[] LiveEnemyPlayers = b.GetEnemyPieces();
 
         Action outgoingAction = AIBishop.BishopAI(b, currentCommander, subordinates, LiveEnemyPlayers);
-        outgoingAction.printAction();
-        b.Move(outgoingAction.getOriginalXCord(), outgoingAction.getOriginalYCord(), outgoingAction.getDestinationXCord(), outgoingAction.getDestinationYCord());
-        Console.WriteLine("New Board:");
-        b.Print();    
+        
+        if (outgoingAction.getIsActing())
+        {
+            outgoingAction.printAction();
+            b.Move(outgoingAction.getOriginalXCord(), outgoingAction.getOriginalYCord(), outgoingAction.getDestinationXCord(), outgoingAction.getDestinationYCord());
+            Console.WriteLine("New Board:");
+            b.Print();    
+        }
+        else 
+        {
+            Console.WriteLine("Valid move was not selected");
+        }
     }
 }
