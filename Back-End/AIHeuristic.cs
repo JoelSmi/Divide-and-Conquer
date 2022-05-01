a41d57220ae8f378fd66a9f07eba4da7a5a5a0da
BigMac1999 <91208688+BigMac1999@users.noreply.github.com>
2022-05-01 12:34:26 -0400

    Update AIHeuristic.cs

--- Back-End/AIHeuristic.cs
+++ Back-End/AIHeuristic.cs
@@ -32,25 +32,25 @@ namespace KingAI1
         public Action[] heuristic0(){
             //This is the first testing heuristic, we want to move P0, P3, and P7.
             //Here we make the commander piece
-            Piece hCommander1 = new Bishop(Color.Black, 0);
             //Here we define all neccessary variables for action 1
+            Piece hCommander1 = new Bishop(Color.Black, 0);
             int[] orig1 = {1,0};
             int[] dest1 = {2,0};
             List<int[]> path1 = new List<int[]>();
             path1.Add(dest1);
-        
+            //Action 2
             Piece hCommander2 = new King(Color.Black);
             int[] orig2 = {1,3};
             int[] dest2 = {2,3};
             List<int[]> path2 = new List<int[]>();
             path2.Add(dest2);
-            
+            //Action 3
             Piece hCommander3 = new Bishop(Color.Black, 0);
             int[] orig3 = {1,6};
             int[] dest3 = {2,6};
             List<int[]> path3 = new List<int[]>();
             path3.Add(dest3);
-
+            //Create and list actions
             Action action1 = new Action(dest1, orig1, 0, typeof(Pawn), hCommander1, path1);
             Action action2 = new Action(dest2, orig2, 0, typeof(Pawn), hCommander2, path2);
             Action action3 = new Action(dest3, orig3, 0, typeof(Pawn), hCommander3, path3);
@@ -61,4 +61,3 @@ namespace KingAI1
         
     }
 }
-
